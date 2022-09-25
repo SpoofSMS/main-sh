6e952f50d917cec9898b314f6f61f1fc261b223d
SpoofSMS <105524232+SpoofSMS@users.noreply.github.com>
2022-09-25 02:37:25 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+sudo add-apt-repository ppa:ondrej/php &&
+sudo apt-get update
+sleep 10 &&
+sudo apt -y install software-properties-common
+sudo apt-get update
+sleep 10
+cd ~
+curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php &&
+sleep 2
+HASH=`curl -sS https://composer.github.io/installer.sig` &&
+echo $HASH && 
+php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" &&
+sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer &&
+sudo apt-get install -y php-cli php-json php-common php-mysql php-zip php-gd php-mbstring php-curl php-xml php-bcmath mysql-server sudo apt install openssl php-common php-curl php-json php-mbstring php-mysql php-xml php-zip php-intl nginx && wait 
